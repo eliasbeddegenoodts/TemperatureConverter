6934200f98fdead107e5d4380ba08928588ee959
eliasbeddegenoodts <r0763390@ucll.be>
2021-03-29 19:45:54 +0200

    ITemperatureScale

---
+++ TemperatureConverter/View/TemperatureScale.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View
+{
+    class TemperatureScale
+    {
+        public interface ITemperatureScale
+        {
+            string Name { get; }
+
+            double ConvertToKelvin(double temperature);
+            double ConvertFromKelvin(double temperature);
+        }
+    }
+}
