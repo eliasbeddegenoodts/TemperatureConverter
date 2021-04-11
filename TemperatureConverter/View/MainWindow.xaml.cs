93fb76c910608440efc567a2f672c64233cc4239
eliasbeddegenoodts <r0763390@ucll.be>
2021-04-11 15:22:14 +0200

    temperature-in-kelvin

--- TemperatureConverter/View/MainWindow.xaml.cs
+++ TemperatureConverter/View/MainWindow.xaml.cs
@@ -13,18 +13,49 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
+using System.ComponentModel;
 
 namespace View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private double temperatureInKelvin;
+
+        public double TemperatureInKelvin
+        {
+            get
+            {
+                return temperatureInKelvin;
+            }
+            set
+            {
+                temperatureInKelvin = value;
+
+                //if (PropertyChanged != null)
+                //{
+                //    PropertyChanged(this, new PropertyChangedEventArgs(nameof(TemperatureInKelvin)));
+                //    // this = indicates whose property has been changed
+                //    // PropertyChangedEventArgs = takes name of property that got changed
+                //    // This is a string, and we could have simply put "TemperatureInKelvin".
+                //    // However, using nameof(TemperatureInKelvin) is much more robust: if we were to refactor 
+                //    // - rename TemperatureInKelvin, the IDE understands this argument has to change too.
+                //    // If it had been a simple string, the IDE would simply ignore it during refactoring.
+                //}
+                // vervangen door
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TemperatureInKelvin)));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 
     public class TempConverter : IValueConverter
