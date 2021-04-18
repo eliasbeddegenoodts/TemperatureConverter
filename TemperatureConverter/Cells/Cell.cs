c5691c0ee253cfac27b4cce12fa2d9df557a197b
eliasbeddegenoodts <r0763390@ucll.be>
2021-04-18 18:13:33 +0200

    Derived get

--- TemperatureConverter/Cells/Cell.cs
+++ TemperatureConverter/Cells/Cell.cs
@@ -16,7 +16,9 @@ namespace Cells
             this.contents = initialContents;
         }
 
-        public T Value
+        public virtual T Value
+        // We want to override the Value property. For this to be possible, C# requires you to declare it
+        // virtual is the base class. virtual basically means "people can override this member in subclasses.
         {
             get
             {
@@ -30,6 +32,47 @@ namespace Cells
             }
         }
 
+        public Cell<U> Derive<U>(Func<T, U> transformer)
+        {
+            return new Derived<T, U>(this, transformer);
+        }
+        // The initial contents of the Derived is specified by : base(transformer(dependency.Value)):
+        // it takes the value of the dependent cell (kelvin) and transforms it (k - 273.15)
+
         public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    public class Derived<IN, OUT>: Cell<OUT>
+        //derived<IN, OUT> = subclass of Cell<OUT>
+
+        //The IN type parameter is the type of the original cell's value (kelvin). The OUT type parameter is the 
+        //type of the new cell (celsius). In our specific case, IN = OUT = double
+    {
+        private readonly Cell<IN> dependency;
+
+        private readonly Func<IN, OUT> transformer;
+
+        public Derived(Cell<IN> dependency, Func<IN, OUT> transformer) : base(transformer(dependency.Value))
+        {
+            this.dependency = dependency;
+            this.transformer = transformer;
+
+            this.dependency.PropertyChanged += (sender, args) => base.Value = transformer(dependency.Value);
+            //base.Value = transformer(dependency.Value) will be executed. In other words, the constructor's
+            //last line states "Each time my dependency's value changes, recompute my own value."
+        }
+
+        public override OUT Value
+        {
+            get
+            {
+                return base.Value;
+            }
+
+            set
+            {
+
+            }
+        }
     }
 }
